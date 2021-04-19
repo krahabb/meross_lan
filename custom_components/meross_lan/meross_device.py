dcd6d0b3b534fe4097b33085d0e3dcadb89bfc34
Davide Perteghella <davide.perteghella@mthree.it>
2021-04-19 20:51:37 +0000

    better support for legacy firmware and disconnects

--- custom_components/meross_lan/meross_device.py
+++ custom_components/meross_lan/meross_device.py
@@ -26,14 +26,18 @@ from .const import (
     NS_APPLIANCE_CONTROL_TOGGLE, NS_APPLIANCE_CONTROL_TOGGLEX,
     NS_APPLIANCE_CONTROL_LIGHT, NS_APPLIANCE_GARAGEDOOR_STATE,
     NS_APPLIANCE_ROLLERSHUTTER_POSITION, NS_APPLIANCE_ROLLERSHUTTER_STATE,
-    NS_APPLIANCE_CONTROL_ELECTRICITY, NS_APPLIANCE_CONTROL_CONSUMPTIONX, NS_APPLIANCE_SYSTEM_ALL,
-    PARAM_UNAVAILABILITY_TIMEOUT, PARAM_ENERGY_UPDATE_PERIOD
+    NS_APPLIANCE_CONTROL_ELECTRICITY, NS_APPLIANCE_CONTROL_CONSUMPTIONX,
+    NS_APPLIANCE_SYSTEM_ALL, NS_APPLIANCE_SYSTEM_REPORT,
+    PARAM_UNAVAILABILITY_TIMEOUT, PARAM_ENERGY_UPDATE_PERIOD, PARAM_UPDATE_POLLING_PERIOD
 )
 
 
 class MerossDevice:
 
     def __init__(self, api: object, device_id: str, entry: ConfigEntry):
+
+        LOGGER.debug("MerossDevice(%s) init", device_id)
+
         self.api = api
         self.entry_id = entry.entry_id
         self.device_id = device_id
@@ -55,51 +59,59 @@ class MerossDevice:
 
         discoverypayload = entry.data.get(CONF_DISCOVERY_PAYLOAD, {})
 
+        self.all = discoverypayload.get("all", {})
         self.ability = discoverypayload.get("ability", {})
 
         try:
             # use a mix of heuristic to detect device features
-            p_all = discoverypayload.get("all", {})
-            p_type = p_all.get("system", {}).get("hardware", {}).get("type", "")
-            digest = p_all.get("digest", {})
+            p_type = self.all.get("system", {}).get("hardware", {}).get("type", "")
 
-            # let's assume lights are controlled by togglex (optimism)
-            light = digest.get("light")
-            if isinstance(light, List):
-                for l in light:
-                    MerossLanLight(self, l.get("channel"))
-            elif isinstance(light, Dict):
-                MerossLanLight(self, light.get("channel", 0))
+            p_digest = self.all.get("digest")
+            if p_digest:
+                light = p_digest.get("light")
+                if isinstance(light, List):
+                    for l in light:
+                        MerossLanLight(self, l.get("channel"))
+                elif isinstance(light, Dict):
+                    MerossLanLight(self, light.get("channel", 0))
 
-            garagedoor = digest.get("garageDoor")
-            if isinstance(garagedoor, List):
-                for g in garagedoor:
-                    MerossLanGarage(self, g.get("channel"))
+                garagedoor = p_digest.get("garageDoor")
+                if isinstance(garagedoor, List):
+                    for g in garagedoor:
+                        MerossLanGarage(self, g.get("channel"))
 
-            # atm we're not sure we can detect this in 'digest' payload
-            if "mrs" in p_type.lower():
-                MerossLanRollerShutter(self, 0)
+                # atm we're not sure we can detect this in 'digest' payload
+                if "mrs" in p_type.lower():
+                    MerossLanRollerShutter(self, 0)
 
-
-            # at any rate: setup switches whenever we find 'togglex'
-            # or whenever we cannot setup anything from digest
-            togglex = digest.get("togglex")
-            if isinstance(togglex, List):
-                for t in togglex:
+                # at any rate: setup switches whenever we find 'togglex'
+                # or whenever we cannot setup anything from digest
+                togglex = p_digest.get("togglex")
+                if isinstance(togglex, List):
+                    for t in togglex:
+                        channel = t.get("channel")
+                        if channel not in self.entities:
+                            MerossLanSwitch(self, channel, self.togglex_set, self.togglex_get)
+                elif isinstance(togglex, Dict):
                     channel = t.get("channel")
                     if channel not in self.entities:
                         MerossLanSwitch(self, channel, self.togglex_set, self.togglex_get)
-            elif isinstance(togglex, Dict):
-                channel = t.get("channel")
-                if channel not in self.entities:
-                    MerossLanSwitch(self, channel, self.togglex_set, self.togglex_get)
-            elif NS_APPLIANCE_CONTROL_TOGGLEX in self.ability:
-                #fallback for switches: in case we couldnt get from NS_APPLIANCE_SYSTEM_ALL
-                if not self.entities:
+
+                #endif p_digest
+
+            # older firmwares (MSS110 with 1.1.28) look like dont really have 'digest'
+            # but have 'control'
+            p_control = self.all.get("control") if p_digest is None else None
+            if p_control:
+                p_toggle = p_control.get("toggle")
+                if isinstance(p_toggle, Dict):
+                    MerossLanSwitch(self, p_toggle.get("channel", 0), self.toggle_set, self.toggle_get)
+
+            #fallback for switches: in case we couldnt get from NS_APPLIANCE_SYSTEM_ALL
+            if not self.entities:
+                if NS_APPLIANCE_CONTROL_TOGGLEX in self.ability:
                     MerossLanSwitch(self, 0, self.togglex_set, self.togglex_get)
-            elif NS_APPLIANCE_CONTROL_TOGGLE in self.ability:
-                #fallback for switches: in case we couldnt get from NS_APPLIANCE_SYSTEM_ALL
-                if not self.entities:
+                elif NS_APPLIANCE_CONTROL_TOGGLE in self.ability:
                     MerossLanSwitch(self, 0, self.toggle_set, self.toggle_get)
 
             if NS_APPLIANCE_CONTROL_ELECTRICITY in self.ability:
@@ -110,10 +122,11 @@ class MerossDevice:
             if NS_APPLIANCE_CONTROL_CONSUMPTIONX in self.ability:
                 self._sensor_energy = MerossLanSensor(self, DEVICE_CLASS_ENERGY, ENERGY_WATT_HOUR)
 
-        except:
-            pass
+            self.mqtt_publish(NS_APPLIANCE_SYSTEM_ALL, METHOD_GET)
 
-        LOGGER.debug("MerossDevice(%s) init", self.device_id)
+        except Exception as e:
+            LOGGER.debug("MerossDevice(%s) init exception:(%s)", device_id, str(e))
+
         return
 
     def __del__(self):
@@ -123,7 +136,7 @@ class MerossDevice:
     @property
     def online(self) -> bool:
         if self._online:
-            #evaluate device MQTT availability by checking lastrequest got answered in less than 10 seconds
+            #evaluate device MQTT availability by checking lastrequest got answered in less than 20 seconds
             if (self.lastupdate > self.lastrequest) or ((time() - self.lastrequest) < PARAM_UNAVAILABILITY_TIMEOUT):
                 return True
             #else
@@ -134,109 +147,124 @@ class MerossDevice:
         return False
 
     def parsepayload(self, namespace: str, method: str, payload: dict, replykey: Union[dict, Optional[str]]) -> None:  # pylint: disable=unsubscriptable-object
-        try:
-            LOGGER.debug("MerossDevice(%s) MQTT recv method:%s namespace:%s ", self.device_id, method, namespace)
+        """
+        every time we receive a response we save it's 'replykey':
+        that would be the same as our self.key (which it is compared against in 'get_replykey')
+        if it's good else it would be the device message header to be used in
+        a reply scheme where we're going to 'fool' the device by using its own hashes
+        if our config allows for that (our self.key is 'None' which means empty key or auto-detect)
+
+        Update: this key trick actually doesnt work on MQTT (but works on HTTP)
+        """
+        self.replykey = replykey
+        if replykey != self.key:
+            LOGGER_trap(WARNING, "Meross device key error for device_id: %s", self.device_id)
+
+        self.lastupdate = time()
+        if not self._online:
+            LOGGER.debug("MerossDevice(%s) back online!", self.device_id)
+            self._online = True
+            if namespace != NS_APPLIANCE_SYSTEM_ALL:
+                self.mqtt_publish(NS_APPLIANCE_SYSTEM_ALL, METHOD_GET)
+            for entity in self.entities.values():
+                entity._set_available()
+
+        # this parsing is going to get 'bolder' as soon as we add more and more messages to parse
+        # this is not optimal since, based on device/hardware we should be able to really restrict
+        # this checks..right now let's go with it and order this check list by the likelyhood
+        # of receiving a particular message (the first should be the more frequent overall and so on...)
+        # This frequency is based on my guess of the actual devices connected to this code:
+        # Most of all should be recent switches
+        # this is naturally valid as per overall users statistic since if you have a particular device
+        # that may be unlucky and always parse down to the last item
+        if namespace == NS_APPLIANCE_CONTROL_TOGGLEX:
+            togglex = payload.get("togglex")
+            if isinstance(togglex, List):
+                for t in togglex:
+                    self.entities[t.get("channel")]._set_onoff(t.get("onoff"))
+            elif isinstance(togglex, Dict):
+                self.entities[togglex.get("channel")]._set_onoff(togglex.get("onoff"))
             """
-            every time we receive a response we save it's 'replykey':
-            that would be the same as our self.key (which it is compared against in 'get_replykey')
-            if it's good else it would be the device message header to be used in
-            a reply scheme where we're going to 'fool' the device by using its own hashes
-            if our config allows for that (our self.key is 'None' which means empty key or auto-detect)
-
-            Update: this key trick actually doesnt work on MQTT (but works on HTTP)
-            """
-            self.replykey = replykey
-            if replykey != self.key:
-                LOGGER_trap(WARNING, "Meross device key error for device_id: %s", self.device_id)
-
-            self.lastupdate = time()
-            if not self._online:
-                LOGGER.debug("MerossDevice(%s) back online!", self.device_id)
-                self._online = True
-                if namespace != NS_APPLIANCE_SYSTEM_ALL:
-                    self.mqtt_publish(NS_APPLIANCE_SYSTEM_ALL, METHOD_GET)
-                for entity in self.entities.values():
-                    entity._set_available()
-
-
-            if namespace == NS_APPLIANCE_CONTROL_TOGGLEX:
-                togglex = payload.get("togglex")
-                if isinstance(togglex, List):
-                    for t in togglex:
-                        self.entities[t.get("channel")]._set_onoff(t.get("onoff"))
-                elif isinstance(togglex, Dict):
-                    self.entities[togglex.get("channel")]._set_onoff(togglex.get("onoff"))
+            # quick refresh power readings after we toggled
+            if NS_APPLIANCE_CONTROL_ELECTRICITY in self.ability:
+                def callme(now):
+                    self._mqtt_publish(NS_APPLIANCE_CONTROL_ELECTRICITY, METHOD_GET)
+                    return
+                # by the look of it meross plugs are not very responsive in updating power readings
+                # most of the times even with 5 secs delay they dont get it right....
+                async_call_later(self.hass, delay = 2, action = callme)
                 """
-                # quick refresh power readings after we toggled
-                if NS_APPLIANCE_CONTROL_ELECTRICITY in self.ability:
-                    def callme(now):
-                        self._mqtt_publish(NS_APPLIANCE_CONTROL_ELECTRICITY, METHOD_GET)
-                        return
-                    # by the look of it meross plugs are not very responsive in updating power readings
-                    # most of the times even with 5 secs delay they dont get it right....
-                    async_call_later(self.hass, delay = 2, action = callme)
-                    """
 
-            elif namespace == NS_APPLIANCE_CONTROL_ELECTRICITY:
-                electricity = payload.get("electricity")
-                power_w = electricity.get("power") / 1000
-                voltage_v = electricity.get("voltage") / 10
-                current_a = electricity.get("current") / 1000
-                if self._sensor_power:
-                    self._sensor_power._set_state(power_w)
-                if self._sensor_current:
-                    self._sensor_current._set_state(current_a)
-                if self._sensor_voltage:
-                    self._sensor_voltage._set_state(voltage_v)
+        elif namespace == NS_APPLIANCE_CONTROL_ELECTRICITY:
+            electricity = payload.get("electricity")
+            power_w = electricity.get("power") / 1000
+            voltage_v = electricity.get("voltage") / 10
+            current_a = electricity.get("current") / 1000
+            if self._sensor_power:
+                self._sensor_power._set_state(power_w)
+            if self._sensor_current:
+                self._sensor_current._set_state(current_a)
+            if self._sensor_voltage:
+                self._sensor_voltage._set_state(voltage_v)
 
-            elif namespace == NS_APPLIANCE_CONTROL_CONSUMPTIONX:
-                if self._sensor_energy:
-                    self.lastupdate_consumption = self.lastupdate
-                    daylabel = strftime("%Y-%m-%d", localtime())
-                    for d in payload.get("consumptionx"):
-                        if d.get("date") == daylabel:
-                            energy_wh = d.get("value")
-                            self._sensor_energy._set_state(energy_wh)
+        elif namespace == NS_APPLIANCE_CONTROL_CONSUMPTIONX:
+            if self._sensor_energy:
+                self.lastupdate_consumption = self.lastupdate
+                daylabel = strftime("%Y-%m-%d", localtime())
+                for d in payload.get("consumptionx"):
+                    if d.get("date") == daylabel:
+                        energy_wh = d.get("value")
+                        self._sensor_energy._set_state(energy_wh)
 
-            elif namespace == NS_APPLIANCE_CONTROL_LIGHT:
-                light = payload.get("light")
-                if isinstance(light, Dict):
-                    self.entities[light.get("channel")]._set_light(light)
+        elif namespace == NS_APPLIANCE_CONTROL_LIGHT:
+            light = payload.get("light")
+            if isinstance(light, Dict):
+                self.entities[light.get("channel")]._set_light(light)
 
-            elif namespace == NS_APPLIANCE_GARAGEDOOR_STATE:
-                garagedoor = payload.get("state")
-                for g in garagedoor:
-                    self.entities[g.get("channel")]._set_open(g.get("open"))
+        elif namespace == NS_APPLIANCE_GARAGEDOOR_STATE:
+            garagedoor = payload.get("state")
+            for g in garagedoor:
+                self.entities[g.get("channel")]._set_open(g.get("open"))
 
-            elif namespace == NS_APPLIANCE_ROLLERSHUTTER_STATE:
-                state = payload.get("state")
-                for s in state:
-                    self.entities[s.get("channel")]._set_rollerstate(s.get("state"))
+        elif namespace == NS_APPLIANCE_ROLLERSHUTTER_STATE:
+            state = payload.get("state")
+            for s in state:
+                self.entities[s.get("channel")]._set_rollerstate(s.get("state"))
 
-            elif namespace == NS_APPLIANCE_ROLLERSHUTTER_POSITION:
-                position = payload.get("position")
-                for p in position:
-                    self.entities[p.get("channel")]._set_rollerposition(p.get("position"))
+        elif namespace == NS_APPLIANCE_ROLLERSHUTTER_POSITION:
+            position = payload.get("position")
+            for p in position:
+                self.entities[p.get("channel")]._set_rollerposition(p.get("position"))
 
-            elif namespace == NS_APPLIANCE_SYSTEM_ALL:
-                digest = payload.get("all", {}).get("digest", {})
-                togglex = digest.get("togglex")
-                if isinstance(togglex, List):
-                    for t in togglex:
+        elif namespace == NS_APPLIANCE_SYSTEM_ALL:
+            self.all = payload.get("all", {})
+            p_digest = self.all.get("digest")
+            if p_digest:
+                p_togglex = p_digest.get("togglex")
+                if isinstance(p_togglex, List):
+                    for t in p_togglex:
                         self.entities[t.get("channel")]._set_onoff(t.get("onoff"))
-                elif isinstance(togglex, Dict):
-                    self.entities[togglex.get("channel")]._set_onoff(togglex.get("onoff"))
-                light = digest.get("light")
-                if isinstance(light, Dict):
-                    self.entities[light.get("channel")]._set_light(light)
-                garagedoor = digest.get("garageDoor")
-                if isinstance(garagedoor, List):
-                    for g in garagedoor:
+                elif isinstance(p_togglex, Dict):
+                    self.entities[p_togglex.get("channel", 0)]._set_onoff(p_togglex.get("onoff"))
+                p_light = p_digest.get("light")
+                if isinstance(p_light, Dict):
+                    self.entities[p_light.get("channel", 0)]._set_light(p_light)
+                p_garagedoor = p_digest.get("garageDoor")
+                if isinstance(p_garagedoor, List):
+                    for g in p_garagedoor:
                         self.entities[g.get("channel")]._set_open(g.get("open"))
+                return
+            # older firmwares (MSS110 with 1.1.28) look like dont really have 'digest'
+            p_control = self.all.get("control")
+            if p_control:
+                p_toggle = p_control.get("toggle")
+                if isinstance(p_toggle, Dict):
+                    self.entities[p_toggle.get("channel", 0)]._set_onoff(p_toggle.get("onoff"))
 
-
-        except:
-            pass
+        elif namespace == NS_APPLIANCE_CONTROL_TOGGLE:
+            p_toggle = payload.get("toggle")
+            if isinstance(p_toggle, Dict):
+                self.entities[p_toggle.get("channel", 0)]._set_onoff(p_toggle.get("onoff"))
 
         return
 
@@ -271,11 +299,9 @@ class MerossDevice:
 
 
     def mqtt_publish(self, namespace: str, method: str, payload: dict = {}):
-        LOGGER.debug("MerossDevice(%s) MQTT send method:%s namespace:%s ", self.device_id, method, namespace)
-        # self.lastrequest should represent the time of the most recent un-responded request
-        if self.lastupdate >= self.lastrequest:
-            self.lastrequest = time()
+        self.lastrequest = time()
         return self.api.mqtt_publish(self.device_id, namespace, method, payload, key=self.replykey if self.key is None else self.key)
+
 
     @callback
     def updatecoordinator_listener(self) -> None:
@@ -291,5 +317,9 @@ class MerossDevice:
         if self._sensor_energy and self._sensor_energy.enabled:
             if ((now - self.lastupdate_consumption) > PARAM_ENERGY_UPDATE_PERIOD):
                 self.mqtt_publish(NS_APPLIANCE_CONTROL_CONSUMPTIONX, METHOD_GET)
+
+        # this is a bit rude atm: we'll keep sending 'heartbeats' to check if the device is still there
+        if (now - self.lastrequest) > PARAM_UPDATE_POLLING_PERIOD:
+            self.mqtt_publish(NS_APPLIANCE_SYSTEM_REPORT, METHOD_GET)
 
         return
