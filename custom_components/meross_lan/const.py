d80dee3a92c2fdfe9128ff3412e3931a2506862c
Davide Perteghella <davide.perteghella@mthree.it>
2021-04-20 21:43:39 +0000

    more commands namespace definitions

--- custom_components/meross_lan/const.py
+++ custom_components/meross_lan/const.py
@@ -2,7 +2,7 @@
 
 DOMAIN = "meross_lan"
 #PLATFORMS = ["switch", "sensor", "light", "cover"]
-
+SERVICE_MQTT_PUBLISH = "mqtt_publish"
 
 CONF_DEVICE_ID = "device_id"
 CONF_KEY = "key"
@@ -21,15 +21,17 @@ METHOD_ERROR = "ERROR"
 
 NS_APPLIANCE_SYSTEM_ALL = "Appliance.System.All"
 NS_APPLIANCE_SYSTEM_ABILITY = "Appliance.System.Ability"
+NS_APPLIANCE_SYSTEM_CLOCK = "Appliance.System.Clock"
 NS_APPLIANCE_SYSTEM_REPORT = "Appliance.System.Report"
 NS_APPLIANCE_SYSTEM_ONLINE = "Appliance.System.Online"
 NS_APPLIANCE_SYSTEM_DEBUG = "Appliance.System.Debug"
 NS_APPLIANCE_CONFIG_TRACE = "Appliance.Config.Trace"
 NS_APPLIANCE_CONFIG_WIFILIST = "Appliance.Config.WifiList"
-NS_APPLIANCE_CONTROL_TOGGLEX = "Appliance.Control.ToggleX"
 NS_APPLIANCE_CONTROL_TOGGLE = "Appliance.Control.Toggle"
+NS_APPLIANCE_CONTROL_TOGGLEX = "Appliance.Control.ToggleX"
 NS_APPLIANCE_CONTROL_TRIGGER = "Appliance.Control.Trigger"
 NS_APPLIANCE_CONTROL_TRIGGERX = "Appliance.Control.TriggerX"
+NS_APPLIANCE_CONTROL_CONSUMPTIONCONFIG = "Appliance.Control.ConsumptionConfig"
 NS_APPLIANCE_CONTROL_CONSUMPTIONX = "Appliance.Control.ConsumptionX"
 NS_APPLIANCE_CONTROL_ELECTRICITY = "Appliance.Control.Electricity"
 # Light Abilities
