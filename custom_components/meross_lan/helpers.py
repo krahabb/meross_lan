8510cc244d630409ca8e24f63a10a20ed1453609
krahabb <davide.perteghella@mthree.it>
2021-08-11 20:57:18 +0000

    fix and improve logger_trap

--- custom_components/meross_lan/helpers.py
+++ custom_components/meross_lan/helpers.py
@@ -5,33 +5,23 @@ import logging
 from time import time
 
 LOGGER = logging.getLogger(__name__[:-8]) #get base custom_component name for logging
-_trap_msg = None
-_trap_args = None
-_trap_time = 0
-_trap_level = 0
+_trap_dict = dict()
 
 def LOGGER_trap(level, timeout, msg, *args):
     """
     avoid repeating the same last log message until something changes or timeout expires
     used mainly when discovering new devices
     """
-    global _trap_msg
-    global _trap_args
-    global _trap_time
-    global _trap_level
+    global _trap_dict
 
     epoch = time()
-    if (_trap_level == level) \
-        and (_trap_msg == msg) \
-        and (_trap_args == args) \
-        and ((epoch - _trap_time) < timeout):
+    trap_key = (msg, *args)
+    trap_time = _trap_dict.get(trap_key, 0)
+    if ((epoch - trap_time) < timeout):
         return
 
     LOGGER.log(level, msg, *args)
-    _trap_msg = msg
-    _trap_args = args
-    _trap_time = epoch
-    _trap_level = level
+    _trap_dict[trap_key] = epoch
 
 
 """
