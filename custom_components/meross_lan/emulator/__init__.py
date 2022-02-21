c9a08477298ba5a08044ba88b49a763b0bcc60b8
krahabb <davide.perteghella@mthree.it>
2022-02-21 22:19:34 +0100

    support DND and json style traces in emulator

--- custom_components/meross_lan/emulator/__init__.py
+++ custom_components/meross_lan/emulator/__init__.py
@@ -1,7 +1,5 @@
-from http.client import HTTPException
 import os
 from time import time
-from urllib.error import HTTPError
 from aiohttp import web
 
 from json import (
@@ -20,27 +18,16 @@ from ..merossclient import (
 class MerossDevice:
 
 
-    def __init__(self, tracefile, uuid, key):
+    def __init__(self, tracefile:str, uuid, key):
         self.uuid = uuid
         self.key = key
         self.namespaces = {}
         with open(tracefile, 'r') as f:
-            for line in f:
-                columns = line.split('\t')
-                #rxtx = columns[1]
-                protocol = columns[-4]
-                method = columns[-3]
-                namespace = columns[-2]
-                data = columns[-1]
-
-                if method == mc.METHOD_GETACK:
-                    if protocol == 'auto':
-                        self.namespaces[namespace] = {
-                            get_namespacekey(namespace): json_loads(data)
-                        }
-                    else:
-                        self.namespaces[namespace] = json_loads(data)
-
+            if tracefile.endswith('.json.txt'):
+                # HA diagnostics trace
+                self._import_json(f)
+            else:
+                self._import_tsv(f)
         # patch system payload with fake ids
         self.descriptor = MerossDeviceDescriptor(self.namespaces[mc.NS_APPLIANCE_SYSTEM_ABILITY])
         self.p_all = self.namespaces[mc.NS_APPLIANCE_SYSTEM_ALL]
@@ -51,8 +38,57 @@ class MerossDevice:
         hardware[mc.KEY_UUID] = self.uuid
         hardware[mc.KEY_MACADDRESS] = self.uuid[-12:]
         self.descriptor.update(self.p_all)
+        if mc.NS_APPLIANCE_SYSTEM_DNDMODE in self.descriptor.ability:
+            self.p_dndmode = { mc.KEY_DNDMODE: { mc.KEY_MODE: 0 }}
 
         print(f"Initialized {self.descriptor.productname} (model:{self.descriptor.productmodel})")
+
+
+    def _import_tsv(self, f):
+        """
+        parse a legacy tab separated values meross_lan trace
+        """
+        for line in f:
+            row = line.split('\t')
+            self._import_tracerow(row)
+
+
+    def _import_json(self, f):
+        """
+        parse a 'diagnostics' HA trace
+        """
+        try:
+            _json = json_loads(f.read())
+            data = _json['data']
+            columns = None
+            for row in data['trace']:
+                if columns is None:
+                    columns = row
+                    # we could parse and setup a 'column search'
+                    # algorithm here should the trace layout change
+                    # right now it's the same as for csv files...
+                else:
+                    self._import_tracerow(row)
+
+        except:
+            pass
+
+        return
+
+
+    def _import_tracerow(self, values: list):
+        #rxtx = values[1]
+        protocol = values[-4]
+        method = values[-3]
+        namespace = values[-2]
+        data = values[-1]
+        if method == mc.METHOD_GETACK:
+            if protocol == 'auto':
+                self.namespaces[namespace] = {
+                    get_namespacekey(namespace): data if isinstance(data, dict) else json_loads(data)
+                }
+            else:
+                self.namespaces[namespace] = data if isinstance(data, dict) else json_loads(data)
 
 
     async def post_config(self, request: web.Request):
@@ -64,7 +100,7 @@ class MerossDevice:
 
         try:
             if namespace not in self.descriptor.ability:
-                raise Exception(f"{namespace} not supported")
+                raise Exception(f"{namespace} not supported in ability")
 
             elif get_replykey(header, self.key) is not self.key:
                 method = mc.METHOD_ERROR
@@ -73,58 +109,8 @@ class MerossDevice:
             elif (handler := getattr(self, f"_{method}_{namespace.replace('.', '_')}", None)) is not None:
                 method, payload = handler(header, payload)
 
-            else:# when no special handler in place we apply a raw euristhic
-
-                try:
-                    n = namespace.split('.')
-                    if n[1] != 'Control':
-                        raise Exception(f"{namespace} not supported")
-
-                    key = get_namespacekey(namespace)
-                    p_payload_key = payload[key]
-                    p_digest = self.p_all_digest
-                    if len(n) == 4:
-                        p_digest = p_digest[n[2].lower()]
-
-                    if key not in p_digest:
-                        raise Exception(f"{key} not present in digest")
-                    p_digest_key = p_digest[key]
-
-                    if method != mc.METHOD_SET:
-                        # TODO.....
-                        raise Exception(f"{method} not supported")
-
-                    def _update(payload: dict):
-                        channel = payload[mc.KEY_CHANNEL]
-                        for p in p_digest_key:
-                            if p[mc.KEY_CHANNEL] == channel:
-                                p.update(payload)
-                                break
-                        else:
-                            raise Exception(f"{channel} not present in digest.{key}")
-
-                    if isinstance(p_digest_key, list):
-                        if isinstance(p_payload_key, list):
-                            for p_p in p_payload_key:
-                                _update(p_p)
-                        else:
-                            _update(p_payload_key)
-                    else:
-                        if p_digest_key[mc.KEY_CHANNEL] == p_payload_key[mc.KEY_CHANNEL]:
-                            p_digest_key.update(p_payload_key)
-                        else:
-                            raise Exception(f"{p_payload_key[mc.KEY_CHANNEL]} not present in digest.{key}")
-
-                    method = mc.METHOD_SETACK
-                    payload = {}
-
-                except Exception as e:
-                    if (method == mc.METHOD_GET) and (namespace in self.namespaces):
-                        method = mc.METHOD_GETACK
-                        payload = self.namespaces[namespace]
-                    else:
-                        raise e
-
+            else:
+                method, payload = self._handler_default(method, namespace, payload)
 
         except Exception as e:
             method = mc.METHOD_ERROR
@@ -134,10 +120,98 @@ class MerossDevice:
         return web.json_response(data)
 
 
+    def _handler_default(self, method: str, namespace: str, payload: dict):
+        """
+        This is an euristhic to try parse a namespace carrying state stored in all->digest
+        """
+        try:
+            n = namespace.split('.')
+            if n[1] != 'Control':
+                raise Exception(f"{namespace} not supported in emulator")
+
+            key = get_namespacekey(namespace)
+            p_payload_key = payload[key]
+            p_digest = self.p_all_digest
+            if len(n) == 4:
+                # 4 parts namespaces usaully access a subkey in digest
+                subkey = n[2].lower()
+                if subkey not in p_digest:
+                    raise Exception(f"{subkey} not present in digest")
+                p_digest = p_digest[subkey]
+
+            if key not in p_digest:
+                raise Exception(f"{key} not present in digest")
+            p_digest_key = p_digest[key]
+
+            if method != mc.METHOD_SET:
+                # TODO.....
+                raise Exception(f"{method} not supported in emulator")
+
+            def _update(payload: dict):
+                channel = payload[mc.KEY_CHANNEL]
+                for p in p_digest_key:
+                    if p[mc.KEY_CHANNEL] == channel:
+                        p.update(payload)
+                        break
+                else:
+                    raise Exception(f"{channel} not present in digest.{key}")
+
+            if isinstance(p_digest_key, list):
+                if isinstance(p_payload_key, list):
+                    for p_p in p_payload_key:
+                        _update(p_p)
+                else:
+                    _update(p_payload_key)
+            else:
+                if p_digest_key[mc.KEY_CHANNEL] == p_payload_key[mc.KEY_CHANNEL]:
+                    p_digest_key.update(p_payload_key)
+                else:
+                    raise Exception(f"{p_payload_key[mc.KEY_CHANNEL]} not present in digest.{key}")
+
+            return mc.METHOD_SETACK, {}
+
+        except Exception as e:
+            if (method == mc.METHOD_GET) and (namespace in self.namespaces):
+                return mc.METHOD_GETACK, self.namespaces[namespace]
+            else:
+                raise e
+
+
     def _GET_Appliance_System_All(self, header, payload):
         if self.p_all_system_time:
             self.p_all_system_time[mc.KEY_TIMESTAMP] = int(time())
         return mc.METHOD_GETACK, self.namespaces[mc.NS_APPLIANCE_SYSTEM_ALL]
+
+
+    def _GET_Appliance_System_DNDMode(self, header, payload):
+        return mc.METHOD_GETACK, self.p_dndmode
+
+
+    def _SET_Appliance_System_DNDMode(self, header, payload):
+        self.p_dndmode = payload
+        return mc.METHOD_SETACK, {}
+
+
+    def _get_control_key(self, key):
+        p_control = self.p_all[mc.KEY_ALL].get(mc.KEY_CONTROL)
+        if p_control is None:
+            raise Exception(f"{mc.KEY_CONTROL} not present")
+        if key not in p_control:
+            raise Exception(f"{key} not present in control")
+        return p_control[key]
+
+
+    def _GET_Appliance_Control_Toggle(self, header, payload):
+        # only acual example of this usage comes from legacy firmwares
+        # carrying state in all->control
+        return mc.METHOD_GETACK, { mc.KEY_TOGGLE: self._get_control_key(mc.KEY_TOGGLE) }
+
+
+    def _SET_Appliance_Control_Toggle(self, header, payload):
+        # only acual example of this usage comes from legacy firmwares
+        # carrying state in all->control
+        self._get_control_key(mc.KEY_TOGGLE)[mc.KEY_ONOFF] = payload[mc.KEY_TOGGLE][mc.KEY_ONOFF]
+        return mc.METHOD_SETACK, {}
 
 
     def _SET_Appliance_Control_Thermostat_Mode(self, header, payload):
@@ -201,10 +275,12 @@ def run(argv):
             fullpath = os.path.join(tracefilepath, f)
             #expect only valid csv files
             f = f.split('.')
-            if f[-1] != 'csv':
+            if f[-1] not in ('csv','txt','json'):
                 continue
 
-            #try to extract relevant parameters from file name
+            # filename could be formatted to carry device definitions parameters:
+            # format the filename like 'xxxwhatever-Kdevice_key-Udevice_id'
+            # this way, parameters will be 'binded' to that trace in an easy way
             _key = key
             uuidsub = uuidsub + 1
             _uuidsub = str(uuidsub)
