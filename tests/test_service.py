8e3d36e0cee58f9de85fdded68c8315f56c832c6
krahabb <13969600+krahabb@users.noreply.github.com>
2023-02-12 20:03:01 +0000

    add service response test

--- tests/test_service.py
+++ tests/test_service.py
@@ -9,7 +9,12 @@ from pytest_homeassistant_custom_component.common import (
     async_fire_time_changed,
     async_fire_mqtt_message,
 )
-from custom_components.meross_lan.const import CONF_DEVICE_ID, DOMAIN, SERVICE_REQUEST
+from custom_components.meross_lan.const import (
+    CONF_DEVICE_ID,
+    CONF_NOTIFYRESPONSE,
+    DOMAIN,
+    SERVICE_REQUEST,
+)
 from custom_components.meross_lan.merossclient import const as mc
 
 from tests.conftest import MQTTMock
@@ -81,6 +86,40 @@ async def test_request_on_device(hass: HomeAssistant, mqtt_patch: MQTTMock, aioc
         await hass.async_block_till_done()
 
         assert initialstate == 1 - digest[mc.KEY_TOGGLEX][0][mc.KEY_ONOFF]
+
+        # this call, should not be routed to mqtt since our device is
+        # emulated in http
+        mqtt_patch.mqtt_publish.assert_not_called()
+
+
+async def test_request_notification(hass: HomeAssistant, mqtt_patch: MQTTMock, aioclient_mock):
+    """
+        Test service calls routed through a device
+    """
+    async with devicecontext(mc.TYPE_MSS310, hass, aioclient_mock) as context:
+
+        device_id = context.device.device_id
+
+        # let the device perform it's poll and come online
+        await context.perform_coldstart()
+
+        # when routing the call through a device the service data 'key' is not used
+        await hass.services.async_call(
+            DOMAIN,
+            SERVICE_REQUEST,
+            service_data={
+                CONF_DEVICE_ID: device_id,
+                mc.KEY_NAMESPACE: mc.NS_APPLIANCE_SYSTEM_ALL,
+                CONF_NOTIFYRESPONSE: True
+            },
+            blocking=True,
+        )
+        # the device api will spawn a task to execute the call on async_http_request
+        await hass.async_block_till_done()
+
+        # we should check the notification has been created but
+        # the test context patches (auto-fixture) the persistent_notification and
+        # I'm right now lazy enough to start managing that
 
         # this call, should not be routed to mqtt since our device is
         # emulated in http
