7c4f4fb3e65614304fe6a9917a835512dd7277b7
krahabb <13969600+krahabb@users.noreply.github.com>
2023-04-15 11:45:07 +0000

    add profile diagnostics test

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -7,14 +7,34 @@ import time
 from homeassistant.core import HomeAssistant
 
 from custom_components.meross_lan import const as mlc
-from custom_components.meross_lan.diagnostics import async_get_device_diagnostics
+from custom_components.meross_lan.diagnostics import (
+    async_get_config_entry_diagnostics,
+    async_get_device_diagnostics,
+)
 from custom_components.meross_lan.merossclient import const as mc
 
 from tests import const as tc, helpers
 
 
-async def test_diagnostics(hass: HomeAssistant, aioclient_mock):
+async def test_mqtthub_diagnostics(
+    hass: HomeAssistant, hamqtt_mock: helpers.HAMQTTMocker
+):
+    async with helpers.MQTTHubEntryMocker(hass) as mqtthub_entry_mock:
+        diagnostic = await async_get_config_entry_diagnostics(
+            hass, mqtthub_entry_mock.config_entry
+        )
+        assert diagnostic
 
+
+async def test_profile_diagnostics(hass: HomeAssistant):
+    async with helpers.ProfileEntryMocker(hass) as profile_entry_mock:
+        diagnostic = await async_get_config_entry_diagnostics(
+            hass, profile_entry_mock.config_entry
+        )
+        assert diagnostic
+
+
+async def test_device_diagnostics(hass: HomeAssistant, aioclient_mock):
     async with helpers.devicecontext(mc.TYPE_MSS310, hass, aioclient_mock) as context:
         await context.perform_coldstart()
 
@@ -26,8 +46,7 @@ async def test_diagnostics(hass: HomeAssistant, aioclient_mock):
         assert diagnostic
 
 
-async def test_tracing(hass: HomeAssistant, aioclient_mock):
-
+async def test_device_tracing(hass: HomeAssistant, aioclient_mock):
     async with helpers.devicecontext(mc.TYPE_MSS310, hass, aioclient_mock) as context:
         await context.perform_coldstart()
 
