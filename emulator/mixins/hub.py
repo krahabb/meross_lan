6dea65ca2c3e2ab1f2f40f5b70bd1ad1e3308c2c
krahabb <13969600+krahabb@users.noreply.github.com>
2024-02-08 13:08:35 +0000

    improve hub emulator behavior (normalization of internal state)

--- emulator/mixins/hub.py
+++ emulator/mixins/hub.py
@@ -62,84 +62,120 @@ class HubMixin(MerossEmulator if typing.TYPE_CHECKING else object):
         digest_subdevices = descriptor.digest[mc.KEY_HUB][mc.KEY_SUBDEVICE]
         namespaces = descriptor.namespaces
         ability = descriptor.ability
-        if mc.NS_APPLIANCE_HUB_MTS100_ALL in ability:
-            mts100_all: list | None = namespaces.setdefault(
-                mc.NS_APPLIANCE_HUB_MTS100_ALL, {mc.KEY_ALL: []}
-            )[mc.KEY_ALL]
-        else:
-            mts100_all = None
-        if mc.NS_APPLIANCE_HUB_MTS100_TEMPERATURE in ability:
-            mts100_temperature: list | None = namespaces.setdefault(
-                mc.NS_APPLIANCE_HUB_MTS100_TEMPERATURE, {mc.KEY_TEMPERATURE: []}
-            )[mc.KEY_TEMPERATURE]
-        else:
-            mts100_temperature = None
-        if mc.NS_APPLIANCE_HUB_SENSOR_ALL in ability:
-            sensor_all: list | None = namespaces.setdefault(
-                mc.NS_APPLIANCE_HUB_SENSOR_ALL, {mc.KEY_ALL: []}
-            )[mc.KEY_ALL]
-        else:
-            sensor_all = None
+        ns_state: dict[str, list[dict]] = {}
+        for namespace in (
+            mc.NS_APPLIANCE_HUB_MTS100_ADJUST,
+            mc.NS_APPLIANCE_HUB_MTS100_ALL,
+            mc.NS_APPLIANCE_HUB_MTS100_MODE,
+            mc.NS_APPLIANCE_HUB_MTS100_SCHEDULEB,
+            mc.NS_APPLIANCE_HUB_MTS100_TEMPERATURE,
+            mc.NS_APPLIANCE_HUB_SENSOR_ADJUST,
+            mc.NS_APPLIANCE_HUB_SENSOR_ALL,
+            mc.NS_APPLIANCE_HUB_ONLINE,
+            mc.NS_APPLIANCE_HUB_TOGGLEX,
+        ):
+            if namespace in ability:
+                key_namespace = NAMESPACE_TO_KEY[namespace]
+                ns_state[namespace] = namespaces.setdefault(
+                    namespace, {key_namespace: []}
+                )[key_namespace]
+
+        # these maps help in generalizing the rules for
+        # digest <-> ns_all payloads structure relationship
+        NS_BASE_TO_DIGEST_MAP: dict[str, str] = {
+            mc.NS_APPLIANCE_HUB_ONLINE: mc.KEY_STATUS,
+            mc.NS_APPLIANCE_HUB_TOGGLEX: mc.KEY_ONOFF,
+        }
+        """digest structure common to both sensors and mtss"""
+        NS_TO_DIGEST_MAP: dict[str, dict[str, str]] = {
+            mc.NS_APPLIANCE_HUB_MTS100_ALL: NS_BASE_TO_DIGEST_MAP
+            | {
+                mc.NS_APPLIANCE_HUB_MTS100_MODE: "",  # "" here means we're not defaulting to a digest key
+                mc.NS_APPLIANCE_HUB_MTS100_TEMPERATURE: "",
+            },
+            mc.NS_APPLIANCE_HUB_SENSOR_ALL: NS_BASE_TO_DIGEST_MAP,
+        }
+        """specialization based on subdevice type for digest <-> ns_all relationship"""
+
         for p_subdevice_digest in digest_subdevices:
             subdevice_id = p_subdevice_digest[mc.KEY_ID]
-            p_mts_digest = get_mts_digest(p_subdevice_digest)
-            if p_mts_digest is not None:
-                # this subdevice is an mts like so we'll ensure it's
-                # 'all' payload (at least) is set
+            # detect first if it0s an mts like or a sensor like
+            if p_subdevice_digest[mc.KEY_STATUS] == mc.STATUS_ONLINE:
+                p_mts_digest = get_mts_digest(p_subdevice_digest)
+                subdevice_ns = (
+                    mc.NS_APPLIANCE_HUB_MTS100_ALL
+                    if p_mts_digest is not None
+                    else mc.NS_APPLIANCE_HUB_SENSOR_ALL
+                )
                 assert (
-                    mts100_all is not None
-                ), f"Hub emulator init: missing {mc.NS_APPLIANCE_HUB_MTS100_ALL}"
+                    subdevice_ns in ns_state
+                ), f"Hub emulator init: missing {subdevice_ns}"
                 p_subdevice_all = get_element_by_key_safe(
-                    mts100_all,
+                    ns_state[subdevice_ns],
                     mc.KEY_ID,
                     subdevice_id,
                 )
-                if not p_subdevice_all:
-                    p_subdevice_all = {mc.KEY_ID: subdevice_id}
-                    if mc.KEY_SCHEDULEBMODE in p_subdevice_digest:
-                        p_subdevice_all[mc.KEY_SCHEDULEBMODE] = p_subdevice_digest[
-                            mc.KEY_SCHEDULEBMODE
-                        ]
-                    if mc.KEY_STATUS in p_subdevice_digest:
-                        p_subdevice_all[mc.KEY_ONLINE] = {
-                            mc.KEY_STATUS: p_subdevice_digest[mc.KEY_STATUS]
-                        }
-                    if mc.KEY_ONOFF in p_subdevice_digest:
-                        p_subdevice_all[mc.KEY_TOGGLEX] = {
-                            mc.KEY_ONOFF: p_subdevice_digest[mc.KEY_ONOFF]
-                        }
-                    if mc.KEY_MODE in p_mts_digest:
-                        p_subdevice_all[mc.KEY_MODE] = {
-                            mc.KEY_STATE: p_mts_digest[mc.KEY_MODE]
-                        }
-                    if mts100_temperature:
-                        # recover the temperature dict for this mts from the (eventual)
-                        # Mts100.temperature namespace query
-                        p_mts100_temperature = get_element_by_key_safe(
-                            mts100_temperature,
+            else:
+                # the p_mts_digest could be missing from digest
+                # when the valve is offline so we'll fallback to inspecting either
+                # MTS100_ALL or SENSOR_ALL for clues..
+                for subdevice_ns in (
+                    mc.NS_APPLIANCE_HUB_MTS100_ALL,
+                    mc.NS_APPLIANCE_HUB_SENSOR_ALL,
+                ):
+                    if subdevice_ns in ns_state:
+                        p_subdevice_all = get_element_by_key_safe(
+                            ns_state[subdevice_ns],
                             mc.KEY_ID,
                             subdevice_id,
                         )
-                        if p_mts100_temperature:
-                            p_mts100_temperature = dict(p_mts100_temperature)
-                            p_mts100_temperature.pop(mc.KEY_ID)
-                            p_subdevice_all[mc.KEY_TEMPERATURE] = p_mts100_temperature
+                        if p_subdevice_all:
+                            break
+                else:
+                    raise Exception(f"Cannot detect type for subdevice {subdevice_id}")
 
-                    mts100_all.append(p_subdevice_all)
-            else:
-                # this subdevice is a sensor like so we'll ensure it's
-                # 'all' payload (at least) is set
-                assert (
-                    sensor_all is not None
-                ), f"Hub emulator init: missing {mc.NS_APPLIANCE_HUB_SENSOR_ALL}"
-                p_subdevice_all = get_element_by_key_safe(
-                    sensor_all,
-                    mc.KEY_ID,
-                    subdevice_id,
-                )
-                if not p_subdevice_all:
-                    # TODO: build a reasonable default
-                    pass
+            # subdevice_ns now tells us if its an mts like or a sensor
+            # p_subdevice_all already carries the ns_all state (if present in trace)
+            if not p_subdevice_all:
+                p_subdevice_all = {mc.KEY_ID: subdevice_id}
+                ns_state[subdevice_ns].append(p_subdevice_all)
+
+            if subdevice_ns is mc.NS_APPLIANCE_HUB_MTS100_ALL:
+                # this subdevice is an mts like so we'll ensure it's
+                # 'all' payload (at least) is set. we'll also bind
+                # the child dicts in 'all' to teh corresponding specific
+                # namespace payload for the subdevice id so that the
+                # state is maintained consistent. For instance, the 'temperature' dict
+                # in the subdevice ns_all payload is the same as the corresponding
+                # payload in Mts100.Temperature
+
+                if mc.KEY_SCHEDULEBMODE in p_subdevice_digest:
+                    p_subdevice_all[mc.KEY_SCHEDULEBMODE] = p_subdevice_digest[
+                        mc.KEY_SCHEDULEBMODE
+                    ]
+
+            for subnamespace, digest_key in NS_TO_DIGEST_MAP[subdevice_ns].items():
+                # here we'll link the specific ns_state to a corresponding
+                # dict in p_subdevice_all payload. This will also
+                # create a default corresponding subdevice ns_state should it be missing
+                if subnamespace in ns_state:
+                    # (sub)namespace is supported in abilities so we'll fix/setup it
+                    key_subnamespace = NAMESPACE_TO_KEY[subnamespace]
+                    p_subdevice_substate = get_element_by_key_safe(
+                        ns_state[subnamespace],
+                        mc.KEY_ID,
+                        subdevice_id,
+                    )
+                    if not p_subdevice_substate:
+                        # we don't have the state in the specific ns
+                        # so we default it eventually initializing with the digest data
+                        p_subdevice_substate = {mc.KEY_ID: subdevice_id}
+                        if digest_key in p_subdevice_digest:
+                            p_subdevice_substate[digest_key] = p_subdevice_digest[
+                                digest_key
+                            ]
+                        ns_state[subnamespace].append(p_subdevice_substate)
+                    p_subdevice_all[key_subnamespace] = p_subdevice_substate
 
     def _get_subdevice_digest(self, subdevice_id: str):
         """returns the subdevice dict from the hub digest key"""
@@ -203,27 +239,29 @@ class HubMixin(MerossEmulator if typing.TYPE_CHECKING else object):
     def _SET_Appliance_Hub_Mts100_Mode(self, header, payload):
         for p_mode in payload[mc.KEY_MODE]:
             subdevice_id = p_mode[mc.KEY_ID]
+            mts_mode = p_mode[mc.KEY_STATE]
             p_subdevice_digest = self._get_subdevice_digest(subdevice_id)
             mts_digest = get_mts_digest(p_subdevice_digest)
-            if mts_digest:
-                if mc.KEY_MODE in mts_digest:
-                    mts_digest[mc.KEY_MODE] = p_mode[mc.KEY_STATE]
-
-            p_subdevice_all = self._get_mts100_all(subdevice_id)
-            if mc.KEY_MODE in p_subdevice_all:
-                # beware the "mode" key in "all" is embedded in the "mode" dict "state" key
-                p_subdevice_all[mc.KEY_MODE][mc.KEY_STATE] = p_mode[mc.KEY_STATE]
-            else:
-                p_subdevice_all[mc.KEY_MODE] = {mc.KEY_STATE: p_mode[mc.KEY_STATE]}
+            if mts_digest and mc.KEY_MODE in mts_digest:
+                mts_digest[mc.KEY_MODE] = mts_mode
 
             p_subdevice_mode = self._get_subdevice_namespace(
                 subdevice_id, mc.NS_APPLIANCE_HUB_MTS100_MODE
             )
-            p_subdevice_mode[mc.KEY_STATE] = p_mode[mc.KEY_STATE]
+            p_subdevice_mode[mc.KEY_STATE] = mts_mode
+
+            if mts_mode in mc.MTS100_MODE_TO_CURRENTSET_MAP:
+                p_subdevice_temperature = self._get_subdevice_namespace(
+                    subdevice_id, mc.NS_APPLIANCE_HUB_MTS100_TEMPERATURE
+                )
+                p_subdevice_temperature[mc.KEY_CURRENTSET] = p_subdevice_temperature[
+                    mc.MTS100_MODE_TO_CURRENTSET_MAP[mts_mode]
+                ]
 
         return mc.METHOD_SETACK, {}
 
     def _SET_Appliance_Hub_Mts100_Temperature(self, header, payload):
+        response_payload = []
         for p_subdevice in payload[mc.KEY_TEMPERATURE]:
             subdevice_id = p_subdevice[mc.KEY_ID]
             p_subdevice_temperature = self._get_subdevice_namespace(
@@ -231,7 +269,17 @@ class HubMixin(MerossEmulator if typing.TYPE_CHECKING else object):
             )
             p_subdevice_temperature.update(p_subdevice)
 
-        return mc.METHOD_SETACK, {}
+            p_subdevice_mode = self._get_subdevice_namespace(
+                subdevice_id, mc.NS_APPLIANCE_HUB_MTS100_MODE
+            )
+            mts_mode = p_subdevice_mode[mc.KEY_STATE]
+            if mts_mode in mc.MTS100_MODE_TO_CURRENTSET_MAP:
+                p_subdevice_temperature[mc.KEY_CURRENTSET] = p_subdevice_temperature[
+                    mc.MTS100_MODE_TO_CURRENTSET_MAP[mts_mode]
+                ]
+            response_payload.append(p_subdevice_temperature)
+
+        return mc.METHOD_SETACK, {mc.KEY_TEMPERATURE: response_payload}
 
     def _GET_Appliance_Hub_Sensor_All(self, header, payload):
         response_payload = self.descriptor.namespaces[mc.NS_APPLIANCE_HUB_SENSOR_ALL]
