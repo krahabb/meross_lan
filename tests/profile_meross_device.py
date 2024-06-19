e678cc5ef6701f0882c08c08d513fb80375df550
krahabb <13969600+krahabb@users.noreply.github.com>
2024-06-19 21:32:08 +0000

    refactor profiling test call

--- tests/profile_meross_device.py
+++ tests/profile_meross_device.py
@@ -1,10 +1,8 @@
 """"""
+
 import cProfile
 import io
 import pstats
-
-# import datetime as dt
-import time
 
 from custom_components.meross_lan.merossclient import const as mc
 
@@ -18,15 +16,11 @@ async def profile_async_request_updates(hass, aioclient_mock, capsys):
         # disable delay in emulator<->aioclient_mock response
         context.emulator_context.frozen_time = None
 
-        # polling_tick = dt.timedelta(seconds=device.polling_period)
-        epoch = time.time()
-
         pr = cProfile.Profile()
         pr.enable()
 
-        for i in range(1000):
-            epoch += device.polling_period
-            await device._async_request_updates(epoch, None)
+        for _ in range(1000):
+            await context.async_poll_single()
 
         pr.disable()
         with capsys.disabled():
@@ -36,16 +30,3 @@ async def profile_async_request_updates(hass, aioclient_mock, capsys):
             print("cProfile sorted stats:")
             ps.print_stats()
             print(iostream.getvalue())
-            # print("cProfile stats:")
-            # pr.print_stats(2)
-
-        """
-        with cProfile.Profile() as pr:
-
-            for i in range(1000):
-                epoch += device.polling_period
-                await device.async_request_updates(epoch, None)
-
-            with capsys.disabled():
-                pr.print_stats()
-        """
