1f0ae79973c17508838763442d3c178dc065546a
krahabb <13969600+krahabb@users.noreply.github.com>
2024-07-22 18:25:17 +0000

    try add a definition for Appliance.Control.ElectricityX namespace (#476)

--- custom_components/meross_lan/merossclient/namespaces.py
+++ custom_components/meross_lan/merossclient/namespaces.py
@@ -213,6 +213,9 @@ Appliance_Control_Diffuser_Spray = _ns_get_push(
 Appliance_Control_Electricity = _ns_get_push(
     mc.NS_APPLIANCE_CONTROL_ELECTRICITY, mc.KEY_ELECTRICITY, _DICT
 )
+Appliance_Control_ElectricityX = _ns_get_push(
+    mc.NS_APPLIANCE_CONTROL_ELECTRICITYX, mc.KEY_ELECTRICITYX, _LIST
+)
 Appliance_Control_Fan = _ns_get(mc.NS_APPLIANCE_CONTROL_FAN, mc.KEY_FAN)
 Appliance_Control_FilterMaintenance = _ns_push(
     mc.NS_APPLIANCE_CONTROL_FILTERMAINTENANCE, mc.KEY_FILTER
