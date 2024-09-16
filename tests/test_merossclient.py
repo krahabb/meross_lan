166180508ef67d3949432a6652073c03a9c97a89
krahabb <13969600+krahabb@users.noreply.github.com>
2024-09-16 18:09:34 +0000

    remove dead code

--- tests/test_merossclient.py
+++ tests/test_merossclient.py
@@ -15,20 +15,7 @@ def test_merossclient_module():
     """
     Test utilities defined in merossclient package/module
     """
-    for mc_symbol in dir(mc):
-        if mc_symbol.startswith("NS_"):
-            namespace = mn.NAMESPACES[getattr(mc, mc_symbol)]
-            _is_hub_namespace = namespace.is_hub
-            if mc_symbol.startswith("NS_APPLIANCE_HUB_"):
-                assert _is_hub_namespace
-            else:
-                assert not _is_hub_namespace
-
-            _is_thermostat_namespace = namespace.is_thermostat
-            if mc_symbol.startswith("NS_APPLIANCE_CONTROL_THERMOSTAT_"):
-                assert _is_thermostat_namespace
-            else:
-                assert not _is_thermostat_namespace
+    pass
 
 
 async def test_cloudapi(hass, cloudapi_mock: helpers.CloudApiMocker):
